0691df0b02b603255bf455387ad207ad667ffb68
asfos22 <asantefoster22@gmail.com>
2023-05-02 12:04:43 +0000

    Purged unnecessary endpoint

--- config/custom/firebase.php
+++ config/custom/firebase.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'fcmURL' => 'https://fcm.googleapis.com/fcm/send',
+    'fcmURL' => '',
     'contentType' =>  'application/json; charset=UTF-8',
-    'authorizationKey' => 'AAAAPrMDLu8:APA91bGLGlHNBQnxyWbRWiSbqUhhiE-jQiu7mjwzlY4j7KIf-U8SuOIvR_mWqDzUErVCL2KMpQ9bfKi0yfepDAZyIe0uBH88puL9a2PHbYr001rPRlqayaZ6ba8ijF7_6u7eAeFd6_A1'
+    'authorizationKey' => ''
 );
