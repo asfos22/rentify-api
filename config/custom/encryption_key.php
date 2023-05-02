f549eda6f9e1c0651936ea2a8e3f12f2c391a720
asfos22 <asantefoster22@gmail.com>
2023-05-02 12:03:49 +0000

    Purged unnecessary array element

--- config/custom/encryption_key.php
+++ config/custom/encryption_key.php
@@ -1,6 +1,3 @@
 <?php
-return array(
-    'auth-token-encryption-key' =>'$nU9ujFblDyvHf$10$15YI5qXex0IMhOwgj3nU9ujFblDyvHfqERtIXnlizdPx/YfFDFR56qERtIXnlizdPx/YfFDFR56FblDyvHfqERtIXnlizdPx/YfFDFR56$2y$10$cSdieCiVaWmot1sXQCV4NfHWUlcSdieCiVaBPudaunsWmot1sX',
-    'confirm-encryption-key' =>   '$2y$10$fmrXOxHSO5gXPjrXjzV$2y$10$fmrXOxHSO5gXPjrXjzVBPudauns$2y$10$fmrXOxHSO5gXPjrXjzVBPudaunsWmot1sXQCV4NfHWUlcSdieCiVaWmot1sXQCV4NfHWUlcSdieCiVaBPudaunsWmot1sXQCV4NfHWUlcSdieCiVa',
-    'forgot-confirm-encryption-key'=>'$jzV$2y$10$fmrXOxHSO5gXPjrXjzVBPudauns$2y$10$fmrXOxHSO5gXPjrXjzVBPudaunsWmot1sXQCV4NfHWUlcSdieCiVaWmot1sXQCV4NfHWUlcSdieCiVaBPudaunsWmot1sXQCV4NfHWUlcSdieCiVa$2y$10$fmrXOxHSO5gRP5i'
-);
+return array(''
+  );
